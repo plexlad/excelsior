a65839afd08c817e216dccc0bacc127bb0a896c8
plexlad <brock@tylerjsmith.me>
2024-01-08 14:07:34 -0700

    Update

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 const config = {
-  defaultTitle: 'Semper Prorsum',
+    defaultTitle: 'Excelsior',
+    embellishedDefaultTitle: '<em>Excelsior!</em>',
+    animatedDefaultTitle: '<em><span>E</span><span>x</span><span>c</span><span>e</span><span>l</span><span>s</span><span>i</span><span>o</span><span>r</span><span>!</span></em>',
+    repositoryLink: '<a href="https://github.com/plexlad/insidetrack">GitHub</a>',
 };
 
 export default config;
